5bdec31a5068a67b1c0c507b34fedd07e0871953
Jose Maria <jmateosmartin@safareyese.es>
2024-11-27 13:29:58 +0100

    Buscador comenzado

--- ComiProyecto/app/src/main/java/com/example/comiproyecto/MainActivity.kt
+++ ComiProyecto/app/src/main/java/com/example/comiproyecto/MainActivity.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -26,6 +27,17 @@ class MainActivity : AppCompatActivity() {
         }
 
 
+        val botonAgregar = findViewById<ImageView>(R.id.botonAgregar)
 
+        botonAgregar.setOnClickListener {
+
+            val intent = Intent(this, MainActivity3::class.java)
+
+            val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+
+            Handler(Looper.getMainLooper()).postDelayed({ pendingIntent.send() }, 0)
+
+
+        }
     }
 }
