f632055c49d5277447de81f6f05e36419c527724
JoseCarlos05 <jgomezbotana@safareyes.es>
2024-11-28 14:23:02 +0100

    Inicio de sesión casi termiado con implementacion de logica

--- ComiProyecto/app/src/main/java/com/example/comiproyecto/InicioSesion.kt
+++ ComiProyecto/app/src/main/java/com/example/comiproyecto/InicioSesion.kt
@@ -1,12 +1,21 @@
 package com.example.comiproyecto
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.comiproyecto.BasedeDatos.BDSQLite
+import com.example.comiproyecto.BasedeDatos.Modelos.Usuario
 
 class InicioSesion : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId", "CommitPrefEdits")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +24,34 @@ class InicioSesion : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val sharedPreferences: SharedPreferences = getSharedPreferences("usuario", MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+
+        val db = BDSQLite(this)
+        val dbW = db.writableDatabase
+        val usuarioBD = Usuario(dbW)
+
+        val correoTexto = findViewById<EditText>(R.id.correoLogin)
+        val contrasenaTexto = findViewById<EditText>(R.id.contrasenaLogin)
+
+        val botonLogin = findViewById<Button>(R.id.botonLogin)
+
+        botonLogin.setOnClickListener {
+
+            val usuario = usuarioBD.buscarUsuarioPorCorreoYContrasena(correoTexto.toString(), contrasenaTexto.toString())
+
+            if (usuario != null) {
+                editor.putInt("usuario", usuario["id"] as Int)
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            } else if () {
+
+            } else {
+                Toast.makeText(this, "Correo o contraseña incorrectos", Toast.LENGTH_LONG).show()
+            }
+
         }
     }
 }
