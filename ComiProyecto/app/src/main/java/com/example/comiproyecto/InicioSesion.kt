4be3cabce4b40099f433cdd2e5bae1739173f392
jesusmj05 <jmorenojimenez@safareyes.es>
2024-12-02 19:07:02 +0100

    Update InicioSesion.kt
    inicio sesion reference

--- ComiProyecto/app/src/main/java/com/example/comiproyecto/InicioSesion.kt
+++ ComiProyecto/app/src/main/java/com/example/comiproyecto/InicioSesion.kt
@@ -14,7 +14,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.comiproyecto.BasedeDatos.BDSQLite
 import com.example.comiproyecto.BasedeDatos.Modelos.Usuario
-import com.example.deportes.R
+import com.example.comiproyecto.R
 
 class InicioSesion : AppCompatActivity() {
     @SuppressLint("MissingInflatedId", "CommitPrefEdits")
